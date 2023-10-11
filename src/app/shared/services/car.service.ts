e247274701c2a422b91b8bc92c134912ccf11297
Deniz Kilicaslan <denizz.kilicaslan@etiya.com>
2023-10-11 19:13:45 +0300

    car components & model

---
+++ src/app/shared/services/car.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Car } from '../models/car';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CarService {
+  private apiUrl = 'http://localhost:3000/cars';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getAllCar(): Observable<Car[]> {
+    let newPath = this.apiUrl;
+    return this.httpClient.get<Car[]>(newPath);
+  }
+
+
+  getCarByBrand(brandId:number):Observable<Car[]>{
+
+    const url = `${this.apiUrl}?model.brandId=${brandId}`;
+    return  this.httpClient.get<Car[]>(url);
+  }
+}
