8bffec5190090fe584309cbd2f18a0d79679c791
Deniz Kilicaslan <denizz.kilicaslan@etiya.com>
2023-10-11 19:17:18 +0300

    pages brand & car

--- src/app/pages/car/car.component.ts
+++ src/app/pages/car/car.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { CarService } from './../../shared/services/car.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Car } from 'src/app/shared/models/car';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+  styleUrls: ['./car.component.css'],
 })
-export class CarComponent {
+export class CarComponent implements OnInit {
+  cars: Car[] = [];
+  textFilterx: string;
+  constructor(
+    private router: Router,
+    private carService: CarService,
+    private activatedRoot: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    this.activatedRoot.params.subscribe((params) => {
+      if (params['id']) {
+        console.log(params['id']);
+        this.getCarByBrandId(params['id']);
+      } else {
+        this.getAllCars();
+      }
+    });
+  }
+
+  getAllCars() {
+    this.carService.getAllCar().subscribe((res) => {
+      this.cars = res;
+    });
+  }
+
+  btnClick() {
+    this.router.navigate(['serap']);
+  }
+
+  getCarByBrandId(brandId: number) {
+    this.carService.getCarByBrand(brandId).subscribe((res) => {
+      this.cars = res;
+    });
+  }
 }
